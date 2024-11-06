e668971684d0df8375a15a6766ced9607a15801e
debbybaby1 <158310739+debbybaby1@users.noreply.github.com>
2024-11-06 09:23:48 +0100

    Add files via upload

---
+++ NodeJs_Backend/app.js
@@ -0,0 +1,14 @@
+const maths = require('./maths')
+
+let first_sum = maths.sum(230,90)
+console.log(first_sum)
+
+let first_subtract = maths.subtract(290,90)
+console.log(first_subtract)
+
+let first_multiply = maths.multiply(200,1000)
+console.log(first_multiply)
+
+let first_division = maths.division(1000,20)
+console.log(first_division)
+
---
+++ NodeJs_Backend/maths.js
@@ -0,0 +1,16 @@
+function sum(a,b){
+    return a + b
+}
+
+function subtract(x, y){
+   return x - y
+}
+
+function multiply(c, d){
+    return c * d
+}
+
+function division(i, j){
+    return i / j
+}
+module.exports = {sum,subtract,multiply,division}
